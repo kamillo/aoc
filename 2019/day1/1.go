6439ea12da783d5a0585f2a7cda38d18f6a2bd49
kamillo <kamilgolunski@gmail.com>
2019-12-01 08:51:47 +0100

    Day 1/2019

---
+++ 2019/day1/1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kamillo/aoc/fileutil"
+	"os"
+	"strconv"
+)
+
+func calcFuel(mass int) int {
+	if mass <= 0 {
+		return 0
+	}
+
+	return mass + calcFuel(mass/3-2)
+}
+
+func main() {
+	lines := fileutil.GetLines(os.Args[1])
+
+	sum1, sum2 := 0, 0
+	for _, line := range lines {
+		mass, _ := strconv.Atoi(line)
+		moduleFuel := mass/3 - 2
+		sum1 += moduleFuel
+		sum2 += calcFuel(moduleFuel)
+	}
+
+	fmt.Println("Part 1: ", sum1)
+	fmt.Println("Part 2: ", sum2)
+}
