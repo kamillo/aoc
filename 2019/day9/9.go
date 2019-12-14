58945452ce905109b87e6926d2a8fdf4d995052d
kamillo <kamilgolunski@gmail.com>
2019-12-14 18:02:51 +0100

    Extract IntCode to separate package

--- 2019/day9/9.go
+++ 2019/day9/9.go
@@ -5,113 +5,9 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/kamillo/aoc/2019/intcode"
 	"github.com/kamillo/aoc/fileutil"
 )
-
-// ABCDE
-// 1002
-//
-// DE - two-digit opcode,      02 == opcode 2
-// C - mode of 1st parameter,  0 == position mode  , 2 == relative mode
-// B - mode of 2nd parameter,  1 == immediate mode
-// A - mode of 3rd parameter,  0 == position mode,
-func runProgram(inCodes []int, input []int, ptrOptional ...int) (int, []int, int) {
-	codes := append([]int(nil), inCodes...)
-	ptr := 0
-	relativeBase := 0
-
-	if len(ptrOptional) > 0 {
-		ptr = ptrOptional[0]
-	}
-	inputPtr := 0
-	ret := 0
-
-	for {
-		pparam1, param2, param3 := 0, 0, 0
-
-		if ptr+1 < len(codes) {
-			pparam1 = ptr + 1
-			if ((codes[ptr] / 100) % 10) == 0 {
-				if codes[ptr+1] < len(codes) {
-					pparam1 = codes[ptr+1]
-				}
-			} else if ((codes[ptr] / 100) % 10) == 2 {
-				if relativeBase+codes[ptr+1] < len(codes) {
-					pparam1 = relativeBase + codes[ptr+1]
-				}
-			}
-		}
-
-		if ptr+2 < len(codes) {
-			param2 = codes[ptr+2]
-			if ((codes[ptr] / 1000) % 10) == 0 {
-				if codes[ptr+2] < len(codes) {
-					param2 = codes[codes[ptr+2]]
-				}
-			} else if ((codes[ptr] / 1000) % 10) == 2 {
-				if relativeBase+codes[ptr+2] < len(codes) {
-					param2 = codes[relativeBase+codes[ptr+2]]
-				}
-			}
-		}
-
-		if ptr+3 < len(codes) {
-			param3 = ptr + 3
-			if ((codes[ptr] / 10000) % 10) == 0 {
-				if codes[ptr+3] < len(codes) {
-					param3 = codes[ptr+3]
-				}
-			} else if ((codes[ptr] / 10000) % 10) == 2 {
-				param3 = relativeBase + codes[ptr+3]
-			}
-		}
-		param1 := codes[pparam1]
-		switch codes[ptr] % 100 {
-		case 1:
-			codes[param3] = param1 + param2
-			ptr += 4
-		case 2:
-			codes[param3] = param1 * param2
-			ptr += 4
-		case 3:
-			codes[pparam1] = input[inputPtr]
-			inputPtr++
-			ptr += 2
-		case 4:
-			//fmt.Println("out: ", param1)
-			ret = param1
-			ptr += 2
-			return param1, codes, ptr
-		case 5: // jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-			ptr += 3
-			if param1 != 0 {
-				ptr = param2
-			}
-		case 6: // jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-			ptr += 3
-			if param1 == 0 {
-				ptr = param2
-			}
-		case 7: // less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-			codes[param3] = 0
-			if param1 < param2 {
-				codes[param3] = 1
-			}
-			ptr += 4
-		case 8: // equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-			codes[param3] = 0
-			if param1 == param2 {
-				codes[param3] = 1
-			}
-			ptr += 4
-		case 9:
-			relativeBase += param1
-			ptr += 2
-		case 99: // halt
-			return ret, codes, -1
-		}
-	}
-}
 
 func main() {
 	lines := fileutil.GetLines("input.txt")
@@ -126,8 +22,10 @@ func main() {
 		ints[i] = value
 	}
 
-	part1, _, _ := runProgram(ints, []int{1})
-	part2, _, _ := runProgram(ints, []int{2})
-	fmt.Println("Part 1: ", part1)
-	fmt.Println("Part 1: ", part2)
+	intCode := intcode.Make(ints)
+	intCode.Put([]int{1})
+	fmt.Println(intCode.Get())
+	intCode = intcode.Make(ints)
+	intCode.Put([]int{2})
+	fmt.Println(intCode.Get())
 }
---
+++ 2019/intcode/intcode.go
@@ -0,0 +1,145 @@
+package intcode
+
+import (
+	"fmt"
+)
+
+// ABCDE
+// 1002
+//
+// DE - two-digit opcode,      02 == opcode 2
+// C - mode of 1st parameter,  0 == position mode  , 2 == relative mode
+// B - mode of 2nd parameter,  1 == immediate mode
+// A - mode of 3rd parameter,  0 == position mode,
+
+type IntCode struct {
+	Codes []int
+	input []int
+	base  int
+	ptr   int
+}
+
+func Make(codes []int) IntCode {
+	i := IntCode{}
+	//i.Codes = make([]int, len(codes))
+	i.Codes = append(i.Codes, codes...)
+	i.input = make([]int, 0)
+	return i
+}
+
+func (c *IntCode) Put(in []int) {
+	c.input = append(c.input, in...)
+}
+
+func (c IntCode) Run(debug bool) {
+	for {
+		if res, state := c.Get(); state != exit {
+			if debug {
+				fmt.Println(res)
+			}
+		} else {
+			return
+		}
+	}
+}
+
+type State int
+
+const (
+	output State = 0
+	exit   State = 2
+)
+
+func (c *IntCode) Get() (out int, state State) {
+	ret := 0
+
+	for {
+		pparam1, param2, param3 := 0, 0, 0
+
+		if c.Codes[c.ptr]%100 == 99 {
+			return ret, exit
+		}
+
+		if c.ptr+1 < len(c.Codes) {
+			pparam1 = c.ptr + 1
+			if ((c.Codes[c.ptr] / 100) % 10) == 0 {
+				if c.Codes[c.ptr+1] < len(c.Codes) {
+					pparam1 = c.Codes[c.ptr+1]
+				}
+			} else if ((c.Codes[c.ptr] / 100) % 10) == 2 {
+				if c.base+c.Codes[c.ptr+1] < len(c.Codes) {
+					pparam1 = c.base + c.Codes[c.ptr+1]
+				}
+			}
+		}
+
+		if c.ptr+2 < len(c.Codes) {
+			param2 = c.Codes[c.ptr+2]
+			if ((c.Codes[c.ptr] / 1000) % 10) == 0 {
+				if c.Codes[c.ptr+2] < len(c.Codes) {
+					param2 = c.Codes[c.Codes[c.ptr+2]]
+				}
+			} else if ((c.Codes[c.ptr] / 1000) % 10) == 2 {
+				if c.base+c.Codes[c.ptr+2] < len(c.Codes) {
+					param2 = c.Codes[c.base+c.Codes[c.ptr+2]]
+				}
+			}
+		}
+
+		if c.ptr+3 < len(c.Codes) {
+			param3 = c.ptr + 3
+			if ((c.Codes[c.ptr] / 10000) % 10) == 0 {
+				if c.Codes[c.ptr+3] < len(c.Codes) {
+					param3 = c.Codes[c.ptr+3]
+				}
+			} else if ((c.Codes[c.ptr] / 10000) % 10) == 2 {
+				param3 = c.base + c.Codes[c.ptr+3]
+			}
+		}
+		param1 := c.Codes[pparam1]
+		switch c.Codes[c.ptr] % 100 {
+		case 1:
+			c.Codes[param3] = param1 + param2
+			c.ptr += 4
+		case 2:
+			c.Codes[param3] = param1 * param2
+			c.ptr += 4
+		case 3:
+			c.Codes[pparam1] = c.input[0]
+			c.input = append(c.input[:0], c.input[1:]...)
+			c.ptr += 2
+		case 4:
+			//fmt.Println("out: ", param1)
+			ret = param1
+			c.ptr += 2
+			return param1, output
+		case 5: // jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+			c.ptr += 3
+			if param1 != 0 {
+				c.ptr = param2
+			}
+		case 6: // jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+			c.ptr += 3
+			if param1 == 0 {
+				c.ptr = param2
+			}
+		case 7: // less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+			c.Codes[param3] = 0
+			if param1 < param2 {
+				c.Codes[param3] = 1
+			}
+			c.ptr += 4
+		case 8: // equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+			c.Codes[param3] = 0
+			if param1 == param2 {
+				c.Codes[param3] = 1
+			}
+			c.ptr += 4
+		case 9:
+			c.base += param1
+			c.ptr += 2
+		case 99: // halt
+			return ret, exit
+		}
+	}
+}
