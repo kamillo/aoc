9766c28663959a1c8c3012aa217afff2cfd925b8
kamillo <kamilgolunski@gmail.com>
2020-12-02 07:02:53 +0100

    [Go] Add day1 2020

---
+++ 2020/day1/day1.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/kamillo/aoc/fileutil"
+)
+
+func main() {
+	lines := fileutil.GetLines("input.txt")
+	first, second := 0.0, 0.0
+
+	numbers := make([]float64, len(lines))
+	for i, line := range lines {
+		numbers[i], _ = strconv.ParseFloat(line, 64)
+	}
+
+	for i, number1 := range numbers {
+		for j, number2 := range numbers {
+			if i != j && first == 0 {
+				if number1+number2 == 2020 {
+					first = number1 * number2
+				}
+			}
+
+			for k, number3 := range numbers {
+				if i != j && j != k && i != k {
+					if number1+number2+number3 == 2020 {
+						second = number1 * number2 * number3
+						break
+					}
+				}
+			}
+			if second != 0 && first != 0 {
+				break
+			}
+		}
+		if second != 0 && first != 0 {
+			break
+		}
+	}
+	fmt.Printf("First part: %f\n", first)
+	fmt.Printf("Second part: %f\n", second)
+}
