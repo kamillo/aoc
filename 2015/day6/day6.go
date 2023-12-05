7a23b1772ef672ac1df6650f5a6c398aaf76dce6
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:18:13 +0100

    [Go] Add day6/2015

---
+++ 2015/day6/day6.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kamillo/aoc/fileutil"
+	"github.com/kamillo/aoc/point"
+)
+
+func main() {
+	lines := fileutil.GetLines("input.txt")
+
+	binaryLights := [1000][1000]bool{}
+	analogLights := [1000][1000]int{}
+	for _, line := range lines {
+		upperLeft, bottomRight := point.Point2D{0, 0}, point.Point2D{0, 0}
+		action := ""
+
+		if _, err := fmt.Sscanf(line, "%s %s %d,%d through %d,%d", &action, &action, &upperLeft.X, &upperLeft.Y, &bottomRight.X, &bottomRight.Y); err != nil {
+			fmt.Sscanf(line, "%s %d,%d through %d,%d", &action, &upperLeft.X, &upperLeft.Y, &bottomRight.X, &bottomRight.Y)
+		}
+
+		for x := upperLeft.X; x <= bottomRight.X; x++ {
+			for y := upperLeft.Y; y <= bottomRight.Y; y++ {
+				switch action {
+				case "toggle":
+					binaryLights[x][y] = !binaryLights[x][y]
+					analogLights[x][y] += 2
+				case "on":
+					binaryLights[x][y] = true
+					analogLights[x][y]++
+				case "off":
+					binaryLights[x][y] = false
+					analogLights[x][y]--
+					if analogLights[x][y] < 0 {
+						analogLights[x][y] = 0
+					}
+				}
+			}
+		}
+	}
+
+	lit := 0
+	brightness := 0
+	for x := range binaryLights {
+		for y := range binaryLights {
+			brightness += analogLights[x][y]
+			if binaryLights[x][y] {
+				lit++
+			}
+		}
+	}
+
+	fmt.Println(lit)
+	fmt.Println(brightness)
+}
