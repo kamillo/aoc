6c40cc05165f6ce663ad4f27689d688d36bb9b47
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:18:04 +0100

    [Go] Add day 3

---
+++ 2020/day3/day3.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kamillo/aoc/fileutil"
+)
+
+type slope struct {
+	right int
+	down  int
+}
+
+func main() {
+	lines := fileutil.GetLines("input.txt")
+
+	fmt.Println("Part 1: ", countTrees(lines, slope{3, 1}))
+
+	// Part 2
+	slopes := []slope{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
+
+	treesProduct := 1
+	for _, slope := range slopes {
+		treesProduct *= countTrees(lines, slope)
+	}
+
+	fmt.Println("Part 2: ", treesProduct)
+}
+
+func countTrees(grid []string, slope slope) (treesCount int) {
+	x, y := 0, 0
+	width := len(grid[0])
+
+	for y < len(grid) {
+		if grid[y][x%width] == '#' {
+			treesCount++
+		}
+		y += slope.down
+		x += slope.right
+	}
+
+	return
+}
