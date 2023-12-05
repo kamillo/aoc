171ed199e7ca40145b14b6c196cd554bac8ebd57
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:22:02 +0100

    Update utils

---
+++ utils/map.go
@@ -0,0 +1,33 @@
+package utils
+
+import "sort"
+
+type PairStringInt struct {
+	Key   string
+	Value int
+}
+
+type PairList []PairStringInt
+
+func (p PairList) Len() int      { return len(p) }
+func (p PairList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool {
+	if p[i].Key == p[j].Key {
+		return p[i].Value < p[j].Value
+	}
+	return p[i].Key == p[j].Key
+}
+
+func SortMapStringInt(m map[string]int) PairList {
+
+	pairs := make(PairList, len(m))
+	i := 0
+	for k, v := range m {
+		pairs[i] = PairStringInt{k, v}
+		i++
+	}
+
+	sort.Sort(pairs)
+
+	return pairs
+}
