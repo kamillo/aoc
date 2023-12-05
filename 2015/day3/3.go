6cfbe0608a376ac39978bff6be2c0bbb8feb211c
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:16:47 +0100

    AoC 2015 day 3

---
+++ 2015/day3/3.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/kamillo/aoc/fileutil"
+)
+
+func main() {
+	lines := fileutil.GetLines(os.Args[1])
+	// lines := [1]string{"^v^v^v^v^v"}
+	{
+		houses := make(map[[2]int]int)
+		// point := [2]int{0,0}
+		x, y := 0, 0
+		houses[[2]int{x, y}]++
+
+		for _, line := range lines {
+			for _, direction := range strings.Split(line, "") {
+				switch direction {
+				case "^":
+					y++
+				case "v":
+					y--
+				case ">":
+					x++
+				case "<":
+					x--
+				}
+
+				houses[[2]int{x, y}]++
+			}
+		}
+
+		// fmt.Println(houses)
+		fmt.Println("Part 1: ", len(houses))
+	}
+
+	{
+		x, y := 0, 0
+		xx, yy := 0, 0
+
+		houses := make(map[[2]int]int)
+		houses[[2]int{x, y}]++
+		houses[[2]int{xx, yy}]++
+
+		xp := &x
+		yp := &y
+
+		toggle := func() {
+			if xp == &x && yp == &y {
+				xp, yp = &xx, &yy
+			} else {
+				xp, yp = &x, &y
+			}
+		}
+		for _, line := range lines {
+			for _, direction := range strings.Split(line, "") {
+				switch direction {
+				case "^":
+					*yp++
+				case "v":
+					*yp--
+				case ">":
+					*xp++
+				case "<":
+					*xp--
+				}
+
+				houses[[2]int{*xp, *yp}]++
+				toggle()
+			}
+		}
+		// fmt.Println(houses)
+		fmt.Println("Part 2: ", len(houses))
+	}
+}
