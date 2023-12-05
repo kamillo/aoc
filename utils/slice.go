a0d6ecc517ae16455241474840ced64763b49b59
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:18:14 +0100

    [Go] Add slice utils

---
+++ utils/slice.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"strconv"
+	"strings"
+)
+
+func SliceRotateBool(image [][]bool) [][]bool {
+	size := len(image)
+	newImage := make([][]bool, size)
+
+	for y := 0; y < size; y++ {
+		newImage[y] = make([]bool, size)
+	}
+
+	for y := range image {
+		for x := range image[y] {
+			newImage[size-x-1][y] = image[y][x]
+		}
+	}
+
+	return newImage
+}
+
+func SliceFlipBool(image [][]bool) [][]bool {
+	size := len(image)
+	newImage := make([][]bool, size)
+
+	for i := 0; i < size; i++ {
+		newImage[i] = make([]bool, size)
+	}
+
+	for y := range image {
+		for x := range image[y] {
+			newImage[y][size-x-1] = image[y][x]
+		}
+	}
+
+	return newImage
+}
+
+func SliceToString(a []int, sep string) string {
+	if len(a) == 0 {
+		return ""
+	}
+
+	b := make([]string, len(a))
+	for i, v := range a {
+		b[i] = strconv.Itoa(v)
+	}
+	return strings.Join(b, sep)
+}
