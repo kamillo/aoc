012413e95bd966da3dc2cba80f4c91ede9298f34
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:21:39 +0100

    [Go] Add day17/2021 pasrt2

--- 2021/day17/day17.go
+++ 2021/day17/day17.go
@@ -7,24 +7,22 @@ import (
 )
 
 func main() {
-	//target area: x=150..193, y=-136..-86
-	//target area: x=20..30, y=-10..-5
 	// targetStart := utils.NewPoint2D(20, -5)
 	// targetEnd := utils.NewPoint2D(30, -10)
-
 	targetStart := utils.NewPoint2D(150, -86)
 	targetEnd := utils.NewPoint2D(193, -136)
-
-	// velocity := utils.NewPoint2D(6, 9)
 
 	fire := func(velocity utils.Point2D) (utils.Point2D, bool) {
 		probe := utils.NewPoint2D(0, 0)
 		max := utils.NewPoint2D(0, 0)
+
 		for {
 			probe.X += velocity.X
 			probe.Y += velocity.Y
 
-			if velocity.X > 0 {
+			if velocity.X == 0 {
+				velocity.X = 0
+			} else if velocity.X > 0 {
 				velocity.X--
 			} else {
 				velocity.X++
@@ -36,19 +34,14 @@ func main() {
 				max.Y = probe.Y
 			}
 
-			//fmt.Println(probe, velocity)
-
-			if probe.X > targetEnd.X || probe.Y < targetEnd.Y {
-				//fmt.Println("MISS ", max)
+			if velocity.Y < 0 && probe.Y < targetEnd.Y {
 				return utils.NewPoint2D(0, 0), false
 			}
 
 			if probe.X >= targetStart.X && probe.X <= targetEnd.X && probe.Y <= targetStart.Y && probe.Y >= targetEnd.Y {
-				//fmt.Println("HIT ", max)
 				return max, true
 			}
 		}
-		return max, false
 	}
 
 	max := 0
