27527756f23c2d31103f560f59cd1497f4826710
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:16:54 +0100

    Day 15/2019

---
+++ 2019/day15/15.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kamillo/aoc/2019/intcode"
+	"github.com/kamillo/aoc/fileutil"
+	"github.com/kamillo/aoc/point"
+	"math"
+)
+
+func Left(p point.Point2D) point.Point2D {
+	return point.New2D(p.X-1, p.Y)
+}
+func Right(p point.Point2D) point.Point2D {
+	return point.New2D(p.X+1, p.Y)
+}
+func Top(p point.Point2D) point.Point2D {
+	return point.New2D(p.X, p.Y+1)
+}
+func Bottom(p point.Point2D) point.Point2D {
+	return point.New2D(p.X, p.Y-1)
+}
+
+func main() {
+	lines := fileutil.GetLines("input.txt")
+	ints := intcode.ParseInput(lines[0])
+	intCode := intcode.Make(ints)
+
+	area := make(map[point.Point2D]int)
+	distances := make(map[point.Point2D]float64)
+
+	var res int
+	var found point.Point2D
+	dist := 0.0
+	dir := 2
+	iter := 0
+	pos := point.New2D(500, 500)
+
+	for {
+		intCode.Put([]int{dir})
+		res, _ = intCode.Get()
+		np := pos
+		switch dir {
+		case 1:
+			np.Y--
+		case 2:
+			np.Y++
+		case 3:
+			np.X--
+		case 4:
+			np.X++
+		}
+
+		area[np] = res
+		switch res {
+		case 0:
+			switch dir {
+			case 1:
+				dir = 4
+			case 2:
+				dir = 3
+			case 3:
+				dir = 1
+			case 4:
+				dir = 2
+			}
+		case 1:
+			distances[pos] = dist
+			dist++
+			if _, ok := distances[np]; ok {
+				dist = math.Min(dist, distances[np])
+			}
+			pos = np
+			switch dir {
+			case 4:
+				dir = 1
+			case 3:
+				dir = 2
+			case 1:
+				dir = 3
+			case 2:
+				dir = 4
+			}
+		case 2:
+			distances[pos] = dist
+			dist++
+			if _, ok := distances[np]; ok {
+				dist = math.Min(dist, distances[np])
+			}
+			pos = np
+			found = pos
+			// break
+		}
+		if iter > 0 && pos.X == 500 && pos.Y == 500 {
+			break
+		}
+		iter++
+	}
+	fmt.Println("Part 1: ", distances[found])
+
+	needsAir := true
+	steps := 0
+	area[found] = 3
+	for needsAir {
+		needsAir = false
+		areaTemp := make(map[point.Point2D]int)
+		for k, v := range area {
+			if v == 1 {
+				needsAir = true
+				neighbors := []point.Point2D{Top(k), Bottom(k), Left(k), Right(k)}
+				for _, n := range neighbors {
+					if area[n] == 3 {
+						v = 3
+					}
+				}
+			}
+			areaTemp[k] = v
+		}
+		steps++
+		area = areaTemp
+	}
+	fmt.Println("Part 2: ", steps)
+}
---
+++ point/point.go
@@ -0,0 +1,21 @@
+package point
+
+type Point2D struct {
+	X, Y int
+}
+
+func New2D(x, y int) Point2D {
+	return Point2D{x, y}
+}
+
+func (p1 Point2D) Add(p2 Point2D) Point2D {
+	return Point2D{p1.X + p2.X, p1.Y + p2.Y}
+}
+
+type Point3D struct {
+	X, Y, Z float64
+}
+
+func New3D(x, y, z float64) Point3D {
+	return Point3D{x, y, z}
+}
