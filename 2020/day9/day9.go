4972590465c6d9859c397e40860d20f950720416
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:18:06 +0100

    [Go] Add day 9

---
+++ 2020/day9/day9.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kamillo/aoc/fileutil"
+	"github.com/kamillo/aoc/mathutils"
+	"strconv"
+)
+
+func main() {
+	lines := fileutil.GetLines("input.txt")
+
+	preambleLength := 25
+	numbers := make([]int, len(lines))
+	invalid := 0
+	for i, line := range lines {
+		numbers[i], _ = strconv.Atoi(line)
+
+		if i >= preambleLength {
+			found := false
+			numbersToCheck := numbers[i-preambleLength : i]
+
+			for j, num := range numbersToCheck {
+				for k, num2 := range numbersToCheck {
+					if j != k && (num+num2 == numbers[i]) {
+						found = true
+						break
+					}
+				}
+				if found {
+					break
+				}
+			}
+			if !found {
+				invalid = numbers[i]
+				fmt.Println("Part 1: ", numbers[i])
+			}
+		}
+	}
+
+	// Part 2
+	for begin, _ := range numbers {
+		for end := begin; end < len(numbers); end++ {
+			sum := 0
+			subrange := numbers[begin:end]
+			for _, num := range subrange {
+				sum += num
+			}
+			if sum == invalid {
+				fmt.Println("Part 2: ", mathutils.MaxInArray(subrange)+mathutils.MinInArray(subrange))
+				return
+			}
+		}
+	}
+}
