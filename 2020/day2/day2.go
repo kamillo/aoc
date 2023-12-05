77e1377d1a3754e188d351b444b23f023c78919e
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:18:03 +0100

    [Go] Add day 2 2020

---
+++ 2020/day2/day2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/kamillo/aoc/fileutil"
+)
+
+func main() {
+	lines := fileutil.GetLines("input.txt")
+
+	goodPasswords := 0
+	goodPasswordsPart2 := 0
+	for _, line := range lines {
+		min, max := 0, 0
+		var char string
+		var password string
+
+		fmt.Sscanf(line, "%d-%d %1s: %s", &min, &max, &char, &password)
+
+		count := strings.Count(password, char)
+		if count >= min && count <= max {
+			goodPasswords++
+		}
+
+		// Part2
+		valid := false
+		if string(password[min-1]) == char {
+			valid = true
+		}
+		if string(password[max-1]) == char {
+			valid = !valid && true
+		}
+
+		if valid {
+			goodPasswordsPart2++
+		}
+	}
+
+	fmt.Println("Part 1: ", goodPasswords)
+	fmt.Println("Part 2: ", goodPasswordsPart2)
+}
