2d2ec1cd07be8c5c46838580599e71c078d8ec2d
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:20:39 +0100

    [Go] Add JoinInts to utils

--- utils/slice.go
+++ utils/slice.go
@@ -60,3 +60,25 @@ func Sum(array []interface{}) int {
 	}
 	return result
 }
+
+func JoinInts(elems []int, sep string) string {
+	switch len(elems) {
+	case 0:
+		return ""
+	case 1:
+		return strconv.Itoa(elems[0])
+	}
+	n := len(sep) * (len(elems) - 1)
+	for i := 0; i < len(elems); i++ {
+		n += len(strconv.Itoa(elems[i]))
+	}
+
+	var b strings.Builder
+	b.Grow(n)
+	b.WriteString(strconv.Itoa(elems[0]))
+	for _, s := range elems[1:] {
+		b.WriteString(sep)
+		b.WriteString(strconv.Itoa(s))
+	}
+	return b.String()
+}
