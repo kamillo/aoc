8384f006b9f59fb1912d61f8576b4b29884a00d4
kamillo <kamilgolunski@gmail.com>
2023-12-05 21:22:05 +0100

    [Go] Add day6/2016

--- 2016/day6/day6.go
+++ 2016/day6/day6.go
@@ -1,9 +1,34 @@
 package main
 
-import "github.com/kamillo/aoc/utils"
+import (
+	"fmt"
+
+	"github.com/kamillo/aoc/utils"
+)
 
 func main() {
 	lines := utils.GetLines("input.txt")
 
-	position := []map[rune]int{}
+	position := [8]map[string]int{}
+
+	for _, line := range lines {
+		for i, r := range line {
+			if position[i] == nil {
+				position[i] = make(map[string]int)
+			}
+
+			position[i][string(r)]++
+		}
+	}
+
+	message := ""
+	message2 := ""
+	for _, p := range position {
+		sorted := utils.SortMapStringInt(p, true)
+		message += sorted[0].Key
+		message2 += sorted[len(sorted)-1].Key
+	}
+
+	fmt.Println("Part 1:", message)
+	fmt.Println("Part 2:", message2)
 }
